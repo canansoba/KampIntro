f79c09d1dac2408155c6c574b64e4a61ca590b44
Canan <Canan@ASUS>
2021-01-20 23:50:33 +0300

    20.01.21

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //hepsini komentle controle kc gei al controle cu
+            //string[] isimler = new string[] {"Canan", "Harun", "Samet", "Esra"};
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Canan", "Harun", "Samet", "Esra" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            Console.WriteLine("------");
+            //Tüm Listeyi Yazdır
+            //isimler2.ForEach(isim => Console.WriteLine(isim));
+            //Console.WriteLine("--------");
+            //foreach (var isim in isimler2)
+            //{
+            //    Console.WriteLine(isim);
+            //}
+            //Listeden istediğin indeksi sil
+            //isimler2.RemoveAt(0);
+            //isimler2.ForEach(isim => Console.WriteLine(isim));
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2; // 2 -> Mobilya
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId=5, 
+                UnitsInStock =5, ProductName="Kalem", UnitPrice=35 };
+            //Pascal Case  //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            //Console.WriteLine(product1.ProductName); //kamera
+
+            /*productManager.Topla2(3,6);
+            int toplamaSonucu = productManager.Topla(3,6);
+            Console.WriteLine(toplamaSonucu*2);           
+            int sonuc = toplamaSonucu * 2;
+            Console.WriteLine(sonuc);*/
+
+            /*int sayi = 100;
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi); //100 */
+
+            //int,double,bool.....değer tip
+            //diziler,class,abstract clas, interface ...referans tip
+
+        }
+    }
+}
